82a31c67b9a2627f76298f497563afd813b3c7ce
Dpbm <dpbm136@gmail.com>
2025-01-05 08:55:46 -0300

    updated setup script

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install r-base r-base-dev curl libcurl4-openssl-dev -y
+sudo apt install r-base r-base-dev curl libcurl4-openssl-dev graphviz -y
 
-curl -L -o /tmp/rstudio.deb https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.09.1-394-amd64.deb
-sudo apt install /tmp/rstudio.deb -y
+
+if [ ! $(which rstudio) &>/dev/null ]
+then
+    echo "Installing RStudio..."
+
+    RSTUDIO_DOWNLOAD_URL = "https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.09.1-394-amd64.deb"
+    TARGET_DEB_FILE_PATH = "/tmp/rstudio.deb"
+
+    curl -L -o $TARGET_DEB_FILE_PATH $RSTUDIO_DOWNLOAD_URL
+    sudo apt install $TARGET_DEB_FILE_PATH -y
+fi
